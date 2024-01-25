d7d256e1438f8c1fe18910882764d13ce369acc9
HyeWon <maru_05@naver.com>
2024-01-25 15:43:14 +0900

    Add c++ module sample
    1. namespace
    2. class

--- framework/ari/source/foo.ixx
+++ framework/ari/source/foo.ixx
@@ -7,3 +7,10 @@ export module helloworld;
 export __declspec(dllexport) void hello() {
   std::cout << "hello,world!" << std::endl;
 }
+
+export namespace A {
+class Test {
+ public:
+  virtual void Print() { std::cout << "test" << std::endl; }
+};
+}  // namespace A
--- source/main.cpp
+++ source/main.cpp
@@ -5,12 +5,20 @@
 
 import helloworld;
 
+class Sub : public A::Test
+{
+ public:
+  virtual void Print() override { std::cout << "sub" << std::endl; }
+};
+
 int main(int argc, int** argv)
 {
 	Ari* ari = Ari::GetInstance();
 	ari->Print();
    
 	hello();
+    A::Test* test = new Sub();
+    test->Print();
 	
 	return 0;
 }
