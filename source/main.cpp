13a99fc53376c1ad98e35856307105a13ed06d7f
HyeWon <maru_05@naver.com>
2024-02-10 20:38:06 +0900

    add asio thread sample code
    1. add asio thread sample code
    2. add compile definitions to pentabong

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,10 @@
 #pragma once
 
 #include <iostream>
+
 #include "ari/ari.h"
 // #include "senbong/senbong.h"
+#include "asio/thread.hpp"
 #include "config_generated/pentabong.pb.h"
 
 import bong;
@@ -11,39 +13,45 @@ import bong;
 import helloworld;
 
 class Sub : public A::Test {
- public:
-  virtual void Print() override { std::cout << "sub" << std::endl; }
+public:
+    virtual void Print() override { std::cout << "sub" << std::endl; }
 };
 
+void thread_test() { std::cout << "thread test" << std::endl; }
+
 int main(int argc, int** argv) {
-  Ari* ari = Ari::GetInstance();
-  ari->Print();
+    Ari* ari = Ari::GetInstance();
+    ari->Print();
 
-  hello();
-  A::Test* test = new Sub();
-  test->Print();
+    hello();
+    A::Test* test = new Sub();
+    test->Print();
 
-  static const std::string senbongPath = "../bin/senbong_d.dll";
+    static const std::string senbongPath = "../bin/senbong_d.dll";
 
-  Bong::Factory factory;
+    Bong::Factory factory;
 
-  bool ret = factory.Load(senbongPath.c_str());
-  if (false == ret) {
+    bool ret = factory.Load(senbongPath.c_str());
+    if (false == ret) {
+        return 0;
+    }
+
+    Bong::Bong* senbong = factory.CreateBong();
+    if (nullptr == senbong) {
+        return 1;
+    }
+
+    senbong->Initialize();
+    senbong->Finalize();
+    senbong->Start();
+    senbong->Stop();
+
+    pentabong::Hello hello;
+    hello.set_text("hello,world");
+    std::cout << hello.text() << std::endl;
+
+    asio::thread t(thread_test);
+    t.join();
+
     return 0;
-  }
-  
-  Bong::Bong* senbong = factory.CreateBong();
-  if (nullptr == senbong) {
-    return 1;
-  }
-
-  senbong->Initialize();
-  senbong->Finalize();
-  senbong->Start();
-  senbong->Stop();
-
-  pentabong::Hello hello;
-  hello.set_text("hello,world");
-  std::cout << hello.text() << std::endl;
-  return 0;
 }
