052c6cc3800a7c58d02c3af0d99a2b80defcee0f
HyeWon <maru_05@naver.com>
2024-03-07 21:06:35 +0900

    adding task sample code
    1. adding task sample code

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,10 @@
 
 #include <iostream>
+#include <queue>
 
 #include "ari/ari.h"
 #include "ari/network.h"
 // #include "senbong/senbong.h"
-#include "asio/thread.hpp"
 #include "asio/io_context.hpp"
 #include "asio/ip/tcp.hpp"
 #include "config_generated/pentabong.pb.h"
@@ -18,10 +18,89 @@ public:
     virtual void Print() override { std::cout << "sub" << std::endl; }
 };
 
-void thread_test() { std::cout << "thread test" << std::endl; }
+class Task {
+public:
+    Task(int id, std::function<void()> task) : _id(id), _task(task) {}
 
-void network_test()
-{
+private:
+    friend class Scheduler;
+    friend class Worker;
+
+    const int _id;
+    const std::function<void()> _task;
+};
+
+class Worker {
+public:
+    Worker() { 
+        _th = new std::thread(&Worker::worker_main, this); }    // asio::thread는 인자 넘길수가 없음.
+
+public:
+    void Post(Task* task) { _taskQueue.push(task);
+    }
+
+private:
+    static void worker_main(Worker* worker) {
+        while (true)
+        {
+            while (!worker->_taskQueue.empty())
+            {
+                auto task = worker->_taskQueue.front();
+                worker->_taskQueue.pop();
+
+                task->_task();
+            }
+    }
+    }
+
+private:
+    std::queue<Task*> _taskQueue;   // 락이 필요하지만 테스트 코드니 일단 쓴다.
+    std::thread* _th;
+};
+
+class Scheduler {
+public:
+    Scheduler(const int count) : _count(count) {
+        for (auto i = 0; i < _count; ++i) {
+            _workerList.push_back(new Worker());
+        }
+    }
+
+public:
+    void Post(Task* task) { int index = task->_id % _count;
+    
+    _workerList[index]->Post(task);
+    }
+
+private:
+    const int _count;
+    std::vector<Worker*> _workerList;
+};
+
+void task_test() {
+    auto scheduler = new Scheduler(4);
+
+    std::cout << "1. " << std::this_thread::get_id() << std::endl;
+
+    int a = 10;
+
+    auto task1 = new Task(1, [a, &scheduler]() {    // 지역변수는 레퍼런스 캡쳐하면 안되지만 테스트 코드니 일단 쓴다.
+        std::cout << "2. " << a << ", " << std::this_thread::get_id()
+                  << std::endl;
+
+        int b = 20;
+        auto task2 = new Task(2, [b]() {
+            std::cout << "3. " << b << ", " << std::this_thread::get_id()
+                      << std::endl;
+        });
+
+        scheduler->Post(task2);
+    });
+
+    scheduler->Post(task1);
+}
+
+void network_test() {
     auto network = ari::Network::TCP(ari::IP::V4, 8080);
     network->Start();
 }
@@ -57,8 +136,7 @@ int main(int argc, int** argv) {
     hello.set_text("hello,world");
     std::cout << hello.text() << std::endl;
 
-    asio::thread t(thread_test);
-    t.join();
+    task_test();
 
     network_test();
 
