f9186b3b98b86a8ad8bfc719f75ff4ca94669d6c
LeoOfSilence <nerostar88@gmail.com>
2024-09-11 20:56:47 +0900

    Adding the data cache logic on Dataloader

--- framework/aoi/include/aoi/aoi.h
+++ framework/aoi/include/aoi/aoi.h
@@ -1,36 +1,83 @@
 #pragma once
+#include <filesystem>  // Add this line
+#include <functional>
 #include <memory>
 #include <mutex>
+#include <typeindex>
+#include <unordered_map>
+
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+
+
+namespace google {
+namespace protobuf {
+class Message;
+}
+}  // namespace google
 
 namespace ari {
 class AriConfig;
 class AriData;
-}
+}  // namespace ari
 
 namespace aoi {
+class MessageFactory {
+public:
+    using Creator = std::function<std::unique_ptr<google::protobuf::Message>()>;
+    // using MessageMap = std::unordered_map<std::string, Creator>;
+    // using MessagePtr = std::unique_ptr<google::protobuf::Message*>;
+
+    static void RegisterMessage(const std::string& typeName, Creator creator);
+    static std::unique_ptr<google::protobuf::Message> CreateMessage(
+        const std::string& typeName);
+
+private:
+    static std::unordered_map<std::string, Creator> _messageMap;
+};
+
 class AOI {
 public:
 private:
     class Deleter {
     public:
-        void operator()(AOI* instance) { delete instance; }
+        void operator()(AOI* instance) {
+            delete instance;
+        }
     };
 
     static std::unique_ptr<AOI, Deleter> _instance;
     static std::once_flag _flag;
     friend Deleter;
 
-    std::unique_ptr<ari::AriConfig> _ariConfig;
-    std::unique_ptr<ari::AriData> _ariData;
+    std::unordered_map<std::type_index,
+                       const std::unique_ptr<google::protobuf::Message>>
+        _messageMap;
 
 public:
+    AOI() = default;
+    AOI(const AOI& other) = delete;
+    AOI& operator=(const AOI& rhs) = delete;
+
+    AOI(AOI&& other) = default;
+    AOI& operator=(AOI&& rhs) = default;
+    virtual ~AOI() = default;
+
     static AOI* GetInstance();
 
     bool Initialize();
     const ari::AriConfig& GetAriConfig() const;
 
 private:
-    bool loadAriConfig(const std::string& rootPath);
-    bool loadAriData(const std::string& rootPath);
+    std::filesystem::path getRootPath(const std::string& additionalPath);
+    bool loadConfig();
+    bool loadDatatable();
+
+    bool loadJson(const std::filesystem::directory_entry& dirEntry);
+
+    bool jsonToMessage(const std::string& jsonString,
+                       google::protobuf::Message& message);
 };
-}
+}  // namespace aoi
--- framework/aoi/source/aoi.cpp
+++ framework/aoi/source/aoi.cpp
@@ -1,22 +1,43 @@
 #include "aoi/aoi.h"
 
 #include <Windows.h>
-
-#include <fstream>
-#include <sstream>
-
-#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/message.h>
 #include <google/protobuf/stubs/status.h>
 #include <google/protobuf/text_format.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/util/json_util.h>
+
+#include <cstdio>
+#include <fstream>
 
 #include "config_generated/ari.pb.h"
 #include "datatable_generated/ari-data.pb.h"
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 std::unique_ptr<aoi::AOI, aoi::AOI::Deleter> aoi::AOI::_instance;
 std::once_flag aoi::AOI::_flag;
+std::unordered_map<std::string, aoi::MessageFactory::Creator>
+    aoi::MessageFactory::_messageMap;
 
 namespace aoi {
+void MessageFactory::RegisterMessage(const std::string& typeName,
+                                     Creator creator) {
+    _messageMap[typeName] = creator;
+}
+
+std::unique_ptr<google::protobuf::Message> MessageFactory::CreateMessage(
+    const std::string& typeName) {
+    auto iter = _messageMap.find(typeName);
+    if (iter == _messageMap.end()) {
+        return nullptr;
+    }
+
+    return iter->second();
+}
+
 AOI* AOI::GetInstance() {
     std::call_once(_flag, [] { _instance.reset(new AOI); });
 
@@ -24,88 +45,114 @@ AOI* AOI::GetInstance() {
 }
 
 bool AOI::Initialize() {
+    MessageFactory::RegisterMessage(
+        "AriConfig", static_cast<MessageFactory::Creator>(
+                         []() -> std::unique_ptr<google::protobuf::Message> {
+                             return std::make_unique<ari::AriConfig>();
+                         }));
+
+    loadConfig();
+    return true;
+}
+
+std::filesystem::path AOI::getRootPath(const std::string& additionalPath) {
     char exePath[MAX_PATH] = {0};
     GetModuleFileName(NULL, exePath, MAX_PATH);
-    std::cout << exePath << std::endl;
 
     std::string::size_type pos = std::string(exePath).find_last_of("\\");
-    if (pos == std::string::npos)
-    {
-        return false;
+    if (pos == std::string::npos) {
+        return std::filesystem::path();
     }
-    
+
     std::string rootPath = std::string(exePath).substr(0, pos);
-    if (false == loadAriConfig(rootPath)) {
+    std::string configDirectoryPath = rootPath;
+    configDirectoryPath.append(additionalPath);
+
+    return std::filesystem::path(configDirectoryPath);
+}
+
+bool AOI::loadConfig() {
+    for (const auto& entry :
+         std::filesystem::directory_iterator(getRootPath("/../data/config/"))) {
+        if (!loadJson(entry)) {
+            continue;
+        }
+    }
+
+    return true;
+}
+
+bool AOI::loadDatatable() {
+    for (const auto& entry : std::filesystem::directory_iterator(
+             getRootPath("/../data/datatable/"))) {
+        if (!loadJson(entry)) {
+            continue;
+        }
+    }
+
+    return true;
+}
+
+bool AOI::loadJson(const std::filesystem::directory_entry& dirEntry) {
+    if (!dirEntry.is_regular_file()) {
         return false;
     }
 
-    if (false == loadAriData(rootPath)) {
+    if (dirEntry.path().extension() != ".json") {
+        return false;
+    }
+    std::ifstream ifs(dirEntry.path());
+    if (!ifs.is_open()) {
+        return false;
+    }
+
+    std::string jsonString((std::istreambuf_iterator<char>(ifs)),
+                           std::istreambuf_iterator<char>());
+    ifs.close();
+
+    rapidjson::Document doc;
+    if (doc.Parse(jsonString.c_str()).HasParseError()) {
+        return false;
+    }
+
+    const rapidjson::Value& name = doc["name"];
+
+    std::cout << name.GetString() << std::endl;
+
+    const rapidjson::Value& rows = doc["rows"][name.GetString()];
+    rapidjson::StringBuffer buffer;
+
+    buffer.Clear();
+
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    rows.Accept(writer);
+    std::cout << buffer.GetString() << std::endl;
+
+    auto config = MessageFactory::CreateMessage(name.GetString());
+    if (!jsonToMessage(buffer.GetString(), *config.get())) {
+        return false;
+    }
+
+    std::type_index key = std::type_index(typeid(*config));
+    auto ret = _messageMap.emplace(key, std::move(config));
+    if (!ret.second) {
         return false;
     }
 
     return true;
 }
 
-const ari::AriConfig& AOI::GetAriConfig() const {
-    return *_ariConfig.get();
-}
+bool AOI::jsonToMessage(const std::string& jsonString,
+                        google::protobuf::Message& message) {
+    google::protobuf::util::JsonParseOptions options;
+    options.ignore_unknown_fields = true;
 
-bool AOI::loadAriConfig(const std::string& rootPath) {
-    ari::AriConfig config;
-    const google::protobuf::Descriptor* desc = config.GetDescriptor();
-
-    std::string fullFilePath = rootPath;
-    fullFilePath.append("/config").append("/").append(desc->name()).append(".json");
-    std::stringstream ss;
-    std::ifstream in(fullFilePath);
-    if (false == in.is_open()) {
+    auto status = google::protobuf::util::JsonStringToMessage(
+        jsonString, &message, options);
+    if (!status.ok()) {
+        std::cerr << "Failed to parse JSON: " << status.ToString() << std::endl;
         return false;
     }
-
-    ss << in.rdbuf();
-    in.close();
-
-    google::protobuf::util::JsonPrintOptions options;
-    options.add_whitespace = true;
-    options.always_print_primitive_fields = true;
-    options.preserve_proto_field_names = true;
-
-    google::protobuf::util::JsonParseOptions options2;
-    JsonStringToMessage(ss.str(), &config, options2);
-
-    _ariConfig.reset(new ari::AriConfig(config));
-
     return true;
 }
-
-bool AOI::loadAriData(const std::string& rootPath) {
-    ari::AriData data;
-    const google::protobuf::Descriptor* desc = data.GetDescriptor();
-
-    std::string fullFilePath = rootPath;
-    fullFilePath.append("/datatable")
-        .append("/")
-        .append(desc->name())
-        .append(".json");
-    std::stringstream ss;
-    std::ifstream in(fullFilePath);
-    if (false == in.is_open()) {
-        return false;
-    }
-
-    ss << in.rdbuf();
-    in.close();
-
-    google::protobuf::util::JsonPrintOptions options;
-    options.add_whitespace = true;
-    options.always_print_primitive_fields = true;
-    options.preserve_proto_field_names = true;
-
-    google::protobuf::util::JsonParseOptions options2;
-    google::protobuf::util::Status status = JsonStringToMessage(ss.str(), &data, options2);
-
-    _ariData.reset(new ari::AriData(data));
-
-    return true;
-}
-}
+}  // namespace aoi
